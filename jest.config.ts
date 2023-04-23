47c58b48b6d30c950791a6a33ee9e820336a136c
JelenaC <jelenacumbo@gmail.com>
2023-04-23 22:25:10 +0200

    Add jest, add sentence converter component and tests.

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  roots: [
+    "<rootDir>/src"
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        // ts-jest configuration goes here
+      },
+    ],
+  },
+  testEnvironment: "jsdom"
+}
+
+export default jestConfig
